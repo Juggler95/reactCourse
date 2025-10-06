2d03b595ce49039ba5d29b616b8728045f0beb48
Juggler95Mac <fireboltisaac@gmail.com>
2025-10-06 13:39:38 -0600

    use axios to fetch the data

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,14 +1,13 @@
+import axios from "axios";
 import { Header } from "../components/header";
 import { products } from "../../starting-code/data/products";
 import "./HomePage.css";
 
 export function HomePage() {
-  fetch('http://localhost:3000/api/products')
+  axios.get("http://localhost:3000/api/products")
     .then((response) => {
-      return response.json()
-    }).then((data) => {
-      console.log(data);
-    })
+    console.log(response.data);
+  });
 
   return (
     <>
@@ -22,10 +21,7 @@ export function HomePage() {
             return (
               <div key={product.id} className="product-container">
                 <div className="product-image-container">
-                  <img
-                    className="product-image"
-                    src={product.image}
-                  />
+                  <img className="product-image" src={product.image} />
                 </div>
 
                 <div className="product-name limit-text-to-2-lines">
@@ -37,10 +33,14 @@ export function HomePage() {
                     className="product-rating-stars"
                     src={`images/ratings/rating-${product.rating.stars * 10}.png`}
                   />
-                  <div className="product-rating-count link-primary">{product.rating.count}</div>
+                  <div className="product-rating-count link-primary">
+                    {product.rating.count}
+                  </div>
                 </div>
 
-                <div className="product-price">${(product.priceCents / 100).toFixed(2)}</div>
+                <div className="product-price">
+                  ${(product.priceCents / 100).toFixed(2)}
+                </div>
 
                 <div className="product-quantity-container">
                   <select>
