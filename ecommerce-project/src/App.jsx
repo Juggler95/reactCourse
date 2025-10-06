d7d16879d4204961c94c4f29b8f54572364ca51a
Juggler95Mac <fireboltisaac@gmail.com>
2025-10-06 15:12:00 -0600

    generate HTML for the cart items

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,5 +1,5 @@
-import axios from 'axios';
-import {useState, useEffect} from 'react'
+import axios from "axios";
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router";
 import { HomePage } from "./pages/HomePage";
 import { CheckoutPage } from "./pages/CheckoutPage";
@@ -11,23 +11,21 @@ import "./App.css";
 function App() {
   const [cart, setCart] = useState([]);
 
-
   useEffect(() => {
-    axios.get("/api/cart-items")
-      .then((response) => {
-        setCart(response.data);
-      })
+    axios.get("/api/cart-items?expand=product").then((response) => {
+      setCart(response.data);
+    });
   }, []);
 
-return (
-  <Routes>
+  return (
+    <Routes>
       <Route index element={<HomePage cart={cart} />} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
-    <Route path="orders" element={<OrdersPage />} />
-    <Route path="tracking" element={<TrackingPage />} />
-    <Route path="*" element={<ErrorPage />} />
-  </Routes>
-);
+      <Route path="orders" element={<OrdersPage />} />
+      <Route path="tracking" element={<TrackingPage />} />
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+  );
 }
 
 export default App;
